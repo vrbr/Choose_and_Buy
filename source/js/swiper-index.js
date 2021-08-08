9d1f7ec328256ee024fc3ff522eaaee7f79118f0
Мария <lpvamaria@gmail.com>
2021-08-08 17:37:50 +0300

    Построена сетка страницы каталога на всех разрешениях

--- source/js/swiper-index.js
+++ source/js/swiper-index.js
@@ -1,21 +1,3 @@
-//menu
-
-const navMain = document.querySelector('.main-nav');
-const navToggle = document.querySelector('.main-nav__toggle');
-
-navMain.classList.remove('main-nav--nojs');
-
-navToggle.addEventListener('click', function() {
-    if (navMain.classList.contains('main-nav--closed')) {
-        navMain.classList.remove('main-nav--closed');
-        navMain.classList.add('main-nav--opened');
-    } else {
-        navMain.classList.add('main-nav--closed');
-        navMain.classList.remove('main-nav--opened');
-    }
-});
-
-//swiper
 const swiper = new Swiper(".mySwiper", {
     slidesToShow: 2,
     slidesToScroll: 1,
